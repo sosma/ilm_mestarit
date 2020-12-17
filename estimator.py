6a22d3a816c66529301a5aa6c5e421b28bdfc06e
osma <okovaskainen@evo.paivola.fi>
2020-12-17 12:00:39 +0200

    add feature selection

--- estimator.py
+++ estimator.py
@@ -4,7 +4,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from itertools import chain, combinations
 from sklearn.linear_model import LogisticRegression
+import time
+
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+def PCA_editor(pca, indexes):
+    for index in indexes:
+        pca.components_[index] = np.zeros(100)
+    return pca
 
 def to01(row):
     if row['class4'] == "nonevent":
@@ -14,7 +24,7 @@ def to01(row):
 
 df = pd.read_csv("npf_train.csv")
 test = pd.read_csv("npf_test_hidden.csv")
-find_numbers = [x for x in df.columns if x.endswith('.mean')]
+find_numbers = [x for x in df.columns if x.endswith('.mean') or x.endswith('.std')]
 
 data_nonscaled = df[find_numbers]
 test_nonscaled = test[find_numbers]
@@ -30,11 +40,13 @@ best_multi = [0, 0]
 best_bin = [0, 0]
 acc_multis = [0] * 50
 acc_bins = [0] * 50
-for i in range(1, 51):
-    n_components = i
-
+start_time = time.time()
+n = 10
+for i in list(powerset(range(n))):
+    n_components = n
     # TODO : test subsets of components
-    pca = PCA(n_components=n_components).fit(pd.concat([test_data, data]))
+    pca_initial = PCA(n_components=n).fit(pd.concat([test_data, data]))
+    pca = PCA_editor(pca_initial, i)
     x_new = pca.transform(data)
     columns = ["pca"+str(x) for x in range(n_components)]
     pca_data = pd.DataFrame(x_new[:,0:n_components], columns=columns)
@@ -55,7 +67,7 @@ for i in range(1, 51):
     clf = LogisticRegression(random_state=0).fit(X_train, y_train)
     acc_multi = (y_test == clf.predict(X_test)).value_counts().loc[True]
     if acc_multi >= best_multi[0]:
-        best_multi = [acc_multi, n_components]
+        best_multi = [acc_multi, i]
 
 
     X_train = train[[x for x in train.columns if x.startswith('pca')]]
@@ -65,7 +77,8 @@ for i in range(1, 51):
     clf = LogisticRegression(random_state=0).fit(X_train, y_train)
     acc_bin = (y_test == clf.predict(X_test)).value_counts().loc[True]
     if acc_bin >= best_bin[0]:
-        best_bin = [acc_bin, n_components]
+        best_bin = [acc_bin, i]
+print("--- %s seconds ---" % (time.time() - start_time))
 
 print("best multi: ", best_multi[0]/215, " correct, n_components = ", best_multi[1])
 print("best binomial: ", best_bin[0]/215, " correct, n_components = ", best_bin[1])
@@ -75,10 +88,11 @@ print("best binomial: ", best_bin[0]/215, " correct, n_components = ", best_bin[
 """
 multiclass logistic regression
 """
-pca_multiclass = PCA(n_components=best_multi[1]).fit(data)
+pca_multiclass = PCA(n_components=n).fit(data)
+pca_multiclass = PCA_editor(pca_multiclass, best_multi[1])
 x_new = pca.transform(data)
-columns = ["pca"+str(x) for x in range(best_multi[1])]
-pca_data = pd.DataFrame(x_new[:,0:best_multi[1]], columns=columns)
+columns = ["pca"+str(x) for x in range(n)]
+pca_data = pd.DataFrame(x_new[:,0:n], columns=columns)
 joint_data = pd.concat([df['class4'],pca_data],axis=1)
 joint_data = joint_data.sample(frac=1).reset_index(drop=True)
 joint_data['class2'] = joint_data.apply(lambda row:  to01(row) , axis=1)
@@ -88,8 +102,8 @@ y_train = joint_data['class4']
 clf = LogisticRegression(random_state=0).fit(X_train, y_train)
 
 x_test = pca.transform(test_data)
-columns = ["pca"+str(x) for x in range(best_multi[1])]
-pca_data = pd.DataFrame(x_test[:,0:best_multi[1]], columns=columns)
+columns = ["pca"+str(x) for x in range(n)]
+pca_data = pd.DataFrame(x_test[:,0:n], columns=columns)
 
 multi_predictions = clf.predict(pca_data)
 multi_predictions_df = pd.DataFrame(multi_predictions, columns = ["class4"])
@@ -98,10 +112,11 @@ multi_predictions_df = pd.DataFrame(multi_predictions, columns = ["class4"])
 """
 binomial logistic regression
 """
-pca_binomial = PCA(n_components=best_bin[1]).fit(data)
+pca_binomial = PCA(n_components=n).fit(data)
+pca_binomial = PCA_editor(pca_binomial, best_bin[1])
 x_new = pca.transform(data)
-columns = ["pca"+str(x) for x in range(best_bin[1])]
-pca_data = pd.DataFrame(x_new[:,0:best_bin[1]], columns=columns)
+columns = ["pca"+str(x) for x in range(n)]
+pca_data = pd.DataFrame(x_new[:,0:n], columns=columns)
 joint_data = pd.concat([df['class4'],pca_data],axis=1)
 joint_data = joint_data.sample(frac=1).reset_index(drop=True)
 joint_data['class2'] = joint_data.apply(lambda row:  to01(row) , axis=1)
@@ -112,8 +127,8 @@ y_train = joint_data['class2']
 clf = LogisticRegression(random_state=0).fit(X_train, y_train)
 
 x_test = pca.transform(test_data)
-columns = ["pca"+str(x) for x in range(best_bin[1])]
-pca_data = pd.DataFrame(x_test[:,0:best_bin[1]], columns=columns)
+columns = ["pca"+str(x) for x in range(n)]
+pca_data = pd.DataFrame(x_test[:,0:n], columns=columns)
 
 binomial_predictions = clf.predict_proba(pca_data)
 binomial_predictions_df = pd.DataFrame(binomial_predictions[:,0], columns = ["p"])
@@ -126,15 +141,15 @@ with open("result.csv", "w") as f:
 # print(result)
 
 # Plotting accuracies with different number of components used
-
-plt.plot(acc_multis)
-plt.title('Multiclass')
-plt.xlabel('Number of components')
-plt.ylabel('Accuracy')
-plt.show()
-
-plt.plot(acc_bins)
-plt.title('Binary')
-plt.xlabel('Number of components')
-plt.ylabel('Accuracy')
-plt.show()
+#
+# plt.plot(acc_multis)
+# plt.title('Multiclass')
+# plt.xlabel('Number of components')
+# plt.ylabel('Accuracy')
+# plt.show()
+#
+# plt.plot(acc_bins)
+# plt.title('Binary')
+# plt.xlabel('Number of components')
+# plt.ylabel('Accuracy')
+# plt.show()
